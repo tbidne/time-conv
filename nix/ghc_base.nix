833e7c59105bff4d1841d562b5f9852325a4bbe0
Tommy Bidne <tbidne@protonmail.com>
2022-07-07 00:05:45 +1200

    Improve ghc_base nixpkgs hash retrieval

--- nix/ghc_base.nix
+++ nix/ghc_base.nix
@@ -1,11 +1,20 @@
 { compilerVersion
-, hash ? (builtins.fromJSON (builtins.readFile ../flake.lock)).nodes.nixpkgs.locked.rev
+, hash ? null
 }:
 
 let
+  hash' =
+    if hash == null
+    then
+      let
+        lockJson = builtins.fromJSON (builtins.readFile ../flake.lock);
+        nixpkgsKey = lockJson.nodes.root.inputs.nixpkgs;
+      in
+      lockJson.nodes.${nixpkgsKey}.locked.rev
+    else hash;
   pkgs = import
     (fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/${hash}.tar.gz";
+      url = "https://github.com/NixOS/nixpkgs/archive/${hash'}.tar.gz";
     })
     { };
   compiler = pkgs.haskell.packages."${compilerVersion}";
