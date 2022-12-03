c5ef944f4e89f040c0a067457dd6d2d07838c12c
Tommy Bidne <tbidne@protonmail.com>
2022-12-04 07:11:06 +1300

    Swap flake-utils for flake-parts

--- flake.nix
+++ flake.nix
@@ -4,42 +4,45 @@
     url = "github:edolstra/flake-compat";
     flake = false;
   };
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-parts.url = "github:hercules-ci/flake-parts";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   outputs =
-    { flake-compat
-    , flake-utils
-    , nixpkgs
+    { flake-parts
     , self
+    , ...
     }:
-    flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs { inherit system; };
-      buildTools = c: with c; [
-        cabal-install
-        pkgs.gnumake
-        pkgs.zlib
-      ];
-      devTools = c: with c; [
-        ghcid
-        haskell-language-server
-      ];
-      ghc-version = "ghc924";
-      compiler = pkgs.haskell.packages."${ghc-version}";
-      mkPkg = returnShellEnv: withDevTools:
-        compiler.developPackage {
-          inherit returnShellEnv;
-          name = "time-conv";
-          root = ./.;
-          modifier = drv:
-            pkgs.haskell.lib.addBuildTools drv
-              (buildTools compiler ++
-                (if withDevTools then devTools compiler else [ ]));
+    flake-parts.lib.mkFlake { inherit self; } {
+      perSystem = { pkgs, ... }:
+        let
+          buildTools = c: with c; [
+            cabal-install
+            pkgs.gnumake
+            pkgs.zlib
+          ];
+          devTools = c: with c; [
+            ghcid
+            haskell-language-server
+          ];
+          ghc-version = "ghc924";
+          compiler = pkgs.haskell.packages."${ghc-version}";
+          mkPkg = returnShellEnv: withDevTools:
+            compiler.developPackage {
+              inherit returnShellEnv;
+              name = "time-conv";
+              root = ./.;
+              modifier = drv:
+                pkgs.haskell.lib.addBuildTools drv
+                  (buildTools compiler ++
+                    (if withDevTools then devTools compiler else [ ]));
+            };
+        in
+        {
+          packages.default = mkPkg false false;
+          devShells.default = mkPkg true true;
+          devShells.ci = mkPkg true false;
         };
-    in
-    {
-      packages.default = mkPkg false false;
-      devShells.default = mkPkg true true;
-      devShells.ci = mkPkg true false;
-    });
+      systems = [
+        "x86_64-linux"
+      ];
+    };
 }
