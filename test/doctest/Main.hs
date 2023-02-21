d6b79e20967776da37c7d368594bd2a572ef81a9
Tommy Bidne <tbidne@protonmail.com>
2023-02-22 10:39:31 +1300

    Swap doctest-parallel for doctest

--- test/doctest/Main.hs
+++ test/doctest/Main.hs
@@ -8,5 +8,44 @@ main =
   guardOrElse'
     "RUN_DOCTEST"
     ExpectEnvSet
-    (DocTest.mainFromCabal "time-conv" [])
+    (DocTest.doctest args)
     (putStrLn "*** Doctests Disabled. Set RUN_DOCTEST=1 to run ***")
+  where
+    args = files <> exts
+
+files :: [String]
+files =
+  [ "-isrc",
+    "src/Data/Time/Conversion/Types.hs",
+    "src/Data/Time/Conversion.hs"
+  ]
+
+exts :: [String]
+exts =
+  [ "-XNoStarIsType",
+    "-XApplicativeDo",
+    "-XDataKinds",
+    "-XDeriveAnyClass",
+    "-XDeriveGeneric",
+    "-XDerivingVia",
+    "-XDuplicateRecordFields",
+    "-XFlexibleContexts",
+    "-XFlexibleInstances",
+    "-XGADTs",
+    "-XImportQualifiedPost",
+    "-XInstanceSigs",
+    "-XLambdaCase",
+    "-XMultiParamTypeClasses",
+    "-XMultiWayIf",
+    "-XNamedFieldPuns",
+    "-XNumericUnderscores",
+    "-XOverloadedLabels",
+    "-XOverloadedStrings",
+    "-XPatternSynonyms",
+    "-XScopedTypeVariables",
+    "-XStandaloneDeriving",
+    "-XStandaloneKindSignatures",
+    "-XTupleSections",
+    "-XTypeApplications",
+    "-XTypeFamilies"
+  ]
