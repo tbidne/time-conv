3720edca255aecac6a531afc84792f2963f2b9e5
Tommy Bidne <tbidne@protonmail.com>
2023-10-04 10:45:58 +1300

    Update haddock script

--- tools/haddock.sh
+++ tools/haddock.sh
@@ -10,4 +10,4 @@ mkdir -p docs/
 # shellcheck disable=SC2038
 find docs/ -type f | xargs -I % sh -c "rm -r %"
 
-cp -r dist-newstyle/build/x86_64-linux/ghc-9.4.4/time-conv-0.1/doc/html/time-conv/* docs/
+cp -r dist-newstyle/build/x86_64-linux/ghc-*/time-conv-0.1/opt/doc/html/time-conv/* docs/
